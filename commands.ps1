b78ed4293bcb496d96bff154ac3ec41926dee8f8
Aliaksey Zmitrykevich <alekseyich@mail.ru>
2019-07-16 14:57:04 +0300

    Update commands.ps1

--- commands.ps1
+++ commands.ps1
@@ -1,8 +1,11 @@
-#To convert a user from UserType Guest to Member
+# To convert a user from UserType Guest to Member
 Install-Module AzureAD
-#Authenticate to your Azure AD tenant
+
+# Authenticate to your Azure AD tenant
 Connect-AzureAD -TenantId <tenant Id>
-#Search for your user by upn (just to be sure)
+
+# Search for your user by upn (just to be sure)
 Get-AzureADUser -SearchString aliaksei_zmitrukevich@epam.com
-#Pass the output to Set-AzureADUser, setting UserType to member
+
+# Pass the output to Set-AzureADUser, setting UserType to member
 Get-AzureADUser -SearchString aliaksei_zmitrukevich@epam.com | Set-AzureADUser -UserType member
